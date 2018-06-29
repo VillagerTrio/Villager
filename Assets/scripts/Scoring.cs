f13a068489c89693113f1ad542f5ae4e86f2c023
Marcos Kichel <marcos.kichel@gmail.com>
2018-06-29 15:20:44 -0300

    Arquivos faltantes

---
+++ Assets/scripts/Scoring.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scoring : MonoBehaviour {
+
+	public Text timeText;
+	public Text scoreText;
+
+	bool started = false;
+	int score = 0;
+	int targetsHitted = 0;
+	float remainingSeconds = 20f;
+
+	// Use this for initialization
+	void Start () {
+		showInstructions ();
+		updateScoreText ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		remainingSeconds -= Time.deltaTime;
+		if (remainingSeconds <= 0f) {
+			inferGameResult ();
+		} else {
+			timeText.text = (int)remainingSeconds + "";
+		}
+	}
+
+	public void addPoints(int howMany) {
+		score += howMany;
+		updateScoreText ();
+	}
+
+	public void newTargetHitted() {
+		targetsHitted ++;
+	}
+
+	void inferGameResult() {
+		if (targetsHitted == 3 && score >= 300) {
+			showSuccess ();
+		} else {
+			showFailure ();
+		}
+	}
+
+	void updateScoreText() {
+		scoreText.text = "Score: " + score;
+	}
+
+	void showInstructions() {
+	}
+
+	void showSuccess() {
+		SceneManager.LoadScene (0);
+	}
+
+	void showFailure() {
+		SceneManager.LoadScene (0);
+	}
+}
---
+++ Assets/scripts/Target.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+	bool hitted = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		GameObject gameObj = GameObject.Find ("Scoring");
+		Scoring scoring = (Scoring) gameObj.GetComponent ((typeof(Scoring)));
+		int points = inferPoints (collider.transform.position.y);
+		scoring.addPoints (points);
+		if (!hitted) {
+			hitted = true;
+			scoring.newTargetHitted ();
+		}
+	}
+
+	int inferPoints(float y) {
+		Debug.Log (y);
+		if (y >= 1.59f && y < 1.71f || y >= 0.9f && y < 1.29f || y < -1.8f && y > -1.45f) {
+			return 50;
+		} else if (y > 1.15f && y < 1.59f || y > 1.71f && y <= 2.1f || y > 1.29f || y > -2.3f || y < 2.0f) {
+			return 30;
+		}
+		return 10;
+	}
+
+}
